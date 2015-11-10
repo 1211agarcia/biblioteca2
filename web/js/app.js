e01b6f3e9177bf0f3a0932fe58b085a3cefcb85b
1211agarcia <1211agarcia@gmail.com>
2015-11-10 09:01:52 -0430

    Loading bar

---
+++ web/js/app.js
@@ -0,0 +1,32 @@
+var tegApp = angular.module('tegApp', ['chieffancypants.loadingBar'])
+  .config(function(cfpLoadingBarProvider) {
+    cfpLoadingBarProvider.includeSpinner = true;
+  });
+tegApp.config(function($interpolateProvider){
+                $interpolateProvider.startSymbol('[[').endSymbol(']]');
+        });
+
+tegApp.controller('LoadCtrl', function ($scope, $http, $timeout, cfpLoadingBar) {
+    $scope.posts = [];
+    $scope.section = null;
+    $scope.subreddit = null;
+    $scope.subreddits = ['cats', 'pics', 'funny', 'gaming', 'AdviceAnimals', 'aww'];
+
+    $scope.start = function() {
+      cfpLoadingBar.start();
+    };
+
+    $scope.complete = function () {
+      cfpLoadingBar.complete();
+    }
+
+
+    // fake the initial load so first time users can see it right away:
+    $scope.start();
+    $scope.fakeIntro = true;
+    $timeout(function() {
+      $scope.complete();
+      $scope.fakeIntro = false;
+    }, 750);
+
+  });
